834a63ece1c2ffd821c1aaa4e1476976e294f968
Matthias Stallmann <feelgood.interface@gmail.com>
2023-11-27 22:32:59 +0100

    add v4 to display name

--- credentials/SuiteCrmV4Api.credentials.ts
+++ credentials/SuiteCrmV4Api.credentials.ts
@@ -1,33 +1,33 @@
-import type { ICredentialType, NodePropertyTypes } from "n8n-workflow";
+import type { ICredentialType, INodeProperties } from 'n8n-workflow';
 
 export class SuiteCrmV4Api implements ICredentialType {
-  name = "suiteCrmV4Api";
-  displayName = "Suite CRM API";
-  properties: INodeProperties[] = [
-    {
-      displayName: "Suite CRM URL",
-      name: "url",
-      type: "string",
-      placeholder: "http://example.com/suitecrm/",
-      default: "",
-      required: true,
-    },
-    // {
-    // 	displayName: 'Username',
-    // 	name: 'user_name',
-    // 	type: 'string' as NodePropertyTypes,
-    // 	default: '',
-    // 	required: true,
-    // },
-    // {
-    // 	displayName: 'Password',
-    // 	name: 'password',
-    // 	type: 'string' as NodePropertyTypes,
-    // 	typeOptions: {
-    // 		password: true,
-    // 	},
-    // 	default: '',
-    // 	required: true,
-    // },
-  ];
+	name = 'suiteCrmV4Api';
+	displayName = 'Suite CRM V4 API';
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Suite CRM URL',
+			name: 'url',
+			type: 'string',
+			placeholder: 'http://example.com/suitecrm/',
+			default: '',
+			required: true,
+		},
+		// {
+		// 	displayName: 'Username',
+		// 	name: 'user_name',
+		// 	type: 'string' as NodePropertyTypes,
+		// 	default: '',
+		// 	required: true,
+		// },
+		// {
+		// 	displayName: 'Password',
+		// 	name: 'password',
+		// 	type: 'string' as NodePropertyTypes,
+		// 	typeOptions: {
+		// 		password: true,
+		// 	},
+		// 	default: '',
+		// 	required: true,
+		// },
+	];
 }
